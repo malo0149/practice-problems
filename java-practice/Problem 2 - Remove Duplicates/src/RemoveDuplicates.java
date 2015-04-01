c7c30c576194ec0e9a3795652e4a59cadd16cac7
Meghan <MeghanAndersonMN@gmail.com>
2015-03-31 21:12:42 -0500

    Problem 2 Part 1: Added JUnit test

--- java-practice/Problem 2 - Remove Duplicates/src/RemoveDuplicates.java
+++ java-practice/Problem 2 - Remove Duplicates/src/RemoveDuplicates.java
@@ -10,25 +10,9 @@ public class RemoveDuplicates {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] originalArray = {1,2,3,2,3,2,5,4,2,1,4,5,10,11,6};
-        int[] modArray = null;
-        if (originalArray != null) {
-            System.out.println("Original Array:");
-            for (int i=0; i<originalArray.length; i++) {
-                System.out.print(originalArray[i] + " ");
-            }
-            System.out.println("\n");
-            modArray = removeDups(originalArray);
-        }
-        if (modArray != null) {
-            System.out.println("Array with Duplicates Removed:");
-            for (int i=0; i<modArray.length; i++) {
-                System.out.print(modArray[i] + " ");
-            }
-            System.out.println("\n");
-        }
+
     }
-    private static int[] removeDups(int[] a) {
+    public static int[] removeDups(int[] a) {
         try {
             List<Integer> aList = new ArrayList<>();
             for (int i : a) {
